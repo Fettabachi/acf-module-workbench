f4a0379732ebd87e601b482f658acea02389f6fe
Tim Fetter <timfettermail@gmail.com>
2026-08-27 18:09:02 -0400

    Add portable content media ACF block

---
+++ inc/blocks.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * ACF block registration.
+ *
+ * @package CR_Practice
+ */
+
+namespace CR_Practice;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Register the theme's ACF blocks from their metadata.
+ */
+function register_acf_blocks(): void {
+	if ( ! function_exists( 'acf_register_block_type' ) ) {
+		return;
+	}
+
+	register_block_type( get_theme_file_path( '/parts/modules/content-media' ) );
+}
+add_action( 'init', __NAMESPACE__ . '\\register_acf_blocks' );
